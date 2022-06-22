4e141deaf2f2a865470ef03c86f8032d360def07
karelito00 <karelkdv2000@gmail.com>
2022-06-22 00:21:27 -0400

    Add word embeddings

---
+++ ir_model/model/word_embeddings/word_embeddings.py
@@ -0,0 +1,41 @@
+import numpy as np
+from scipy import spatial
+from sklearn.neighbors import KDTree
+
+VECTOR_DIMENSION = 50
+
+class WordEmbeddings:
+    def __init__(self):
+        self.embed_dict = {}
+        self.vector_dimension = VECTOR_DIMENSION
+
+        with open(f"./glove6B/glove.6B.{VECTOR_DIMENSION}d.txt", 'r') as f:
+            for line in f:
+                values = line.split()
+                word = values[0]
+                vector = np.array(values[1:], 'float32')
+                self.embed_dict[word] = vector
+        
+        self.kd_tree = KDTree(list(self.embed_dict.values()))
+        self.keys = list(self.embed_dict.keys())
+
+    def vectors_distance(self, vector_a, vector_b):
+        return spatial.distance.euclidean(vector_a, vector_b)
+
+    # brute force
+    def find_similar_word(self, word, count = 2):
+        embed = self.embed_dict[word]
+
+        nearest = sorted(self.embed_dict.keys(), key=lambda word_a: \
+                         self.vectors_distance(self.embed_dict[word_a], embed))
+        return nearest[:count]
+
+    # kdtree
+    def find_similar_word_kdtree(self, word, count = 2):
+        try:
+            embed = self.embed_dict[word]
+        except:
+            return []
+
+        _, nearests = self.kd_tree.query([embed], k = count)
+        return [self.keys[nearest] for nearest in nearests[0]]
