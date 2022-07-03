da050c8e4b2878a3b7ff5fcc7a72ab9dcabe871f
FrankBlanco1 <frank.blanco@estudiantes.matcom.uh.cu>
2022-07-03 16:35:01 -0400

    Server recieve a docs collection name

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,12 @@ FROM docker.uclv.cu/python:3.8.10
 WORKDIR /app
 
 COPY requirements.txt /app
+COPY install.py /app
 
 RUN pip install --no-cache-dir -r requirements.txt \
         --index-url http://nexus.prod.uci.cu/repository/pypi-proxy/simple/ \
         --trusted-host nexus.prod.uci.cu
+        
+RUN python3 install.py
 
 COPY . .
--- run_server.py
+++ run_server.py
@@ -1,6 +1,16 @@
 from distributed.server import Server
+from argparse import ArgumentParser
 
-server = Server('docs-lisa')
+parser = ArgumentParser()
+
+parser.add_argument(
+    '--collection', type=str, required=True,
+    help='The collection of the document that the server goes to process'
+)
+
+args = parser.parse_args()
+
+server = Server(args.collection)
 
 try:
     server.start()
